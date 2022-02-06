a7b4233e690d73edfd7a7f5ca9c160cac36c3554
deniz özkaya <69520906+lacteavian@users.noreply.github.com>
2022-02-06 18:23:03 +0300

    Create proje.py

---
+++ proje.py
@@ -0,0 +1,18 @@
+originallist=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+newlist=[]
+def flattenlist(x):
+    for i in x:
+        if type(i) == list:
+            flattenlist(i)
+        else:
+            newlist.append(i)
+    return newlist
+print(flattenlist(originallist))
+
+list1=[[1, 2], [3, 4], [5, 6, 7]]
+list2=[]
+def reverselist(y):
+    for i in sorted(y,reverse=True):
+        list2.append(sorted(i, reverse=True))
+    return print(list2)
+reverselist(list1)
